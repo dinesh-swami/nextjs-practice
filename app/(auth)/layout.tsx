92d19ce0565727c39146431a0ac0dbd801283732
Dinesh Swami <dinesh907989@gmail.com>
2026-06-11 14:07:19 +0530

    add

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+
+      <h1>auth layout page</h1>
+      {children}
+    </div>
+  )
+}
+
+export default layout
