8b4f7115c4b38886a46e97658339e73809e1be9a
Dinesh Swami <dinesh907989@gmail.com>
2026-06-11 20:12:53 +0530

    add

---
+++ app/api/todos/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse, NextRequest } from "next/server";
+import { prisma } from "../../../lib/db";
+import { Erica_One } from "next/font/google";
+
+export async function GET() {
+  try {
+    const todos = await prisma.todo.findMany();
+    return NextResponse.json(
+      { success: true, data: todos },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { success: false, message: "failed to fetched todos" },
+      {
+        status: 400,
+      },
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { title } = await request.json();
+    if (!title) {
+      return NextResponse.json(
+        {
+          success: true,
+          message: "title is required!",
+        },
+        { status: 400 },
+      );
+    }
+    const todo = await prisma.todo.create({
+      data: {
+        title: title,
+      },
+    });
+    return NextResponse.json(
+      { success: true, data: todo },
+      {
+        status: 201,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { success: false, message: "failed to create todo" },
+      {
+        status: 401,
+      },
+    );
+  }
+}
